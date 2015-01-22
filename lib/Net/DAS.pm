3d6452cefb35fd9763f877675af29dc4c9bc9a53
Michael Holloway <michael@thedarkwinter.com>
2015-01-22 15:54:02 +0100

    version 0.17, fixed manifest, cpan version

--- lib/Net/DAS.pm
+++ lib/Net/DAS.pm
@@ -41,7 +41,7 @@ use Module::Load;
 use IO::Socket::INET;
 use Time::HiRes qw (usleep);
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 our @modules = qw (EU BE NO LT UK SI IT GENT SE NU);
 
 =pod
